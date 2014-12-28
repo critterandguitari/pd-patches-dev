44f3342d17a4883014331a76e6f789261ad3039d
Owen <owen@Owens-MacBook-Pro.local>
2014-12-28 00:16:14 -0500

    change button to key, other changes, whatever

--- gui.py
+++ gui.py
@@ -1,9 +1,19 @@
-from Tkinter import * 
-
+#from Tkinter import * 
+import Tkinter 
+import os
 import socket
+import subprocess
+
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
+
+#pd = subprocess.Popen(["/Applications/Pd-0.46-4.app/Contents/MacOS/Pd", "/Users/owen/pd-patches-dev/patches/mother.pd"])
+
+# TODO :  make helper module, include functions like these :
+def get_immediate_subdirectories(dir):
+    return [name for name in os.listdir(dir)
+            if os.path.isdir(os.path.join(dir, name))]
 
 class Slider:
     def __init__(self, canvas, name, x0, y0, w, h, callback):
@@ -31,7 +41,7 @@ class Slider:
         self.callback(self.name, float(val) / self.height)
         event.widget.coords(self.sliderbgid, self.x0, self.y0, self.x1, newy)
 
-class Button:
+class Key:
     def __init__(self, canvas, name, x0, y0, w, h, callback):
         self.name = name
         self.x0 = x0
@@ -44,63 +54,94 @@ class Button:
         canvas.tag_bind(self.buttonid, '<ButtonRelease-1>', self.onrelease)
     
     def onclick(self, event):  
-        self.callback(self.name, 100)
+        self.callback(self.name, 1)
         canv.itemconfigure(self.buttonid, fill='blue')  
         
     def onrelease(self, event):
         self.callback(self.name, 0)
         canv.itemconfigure(self.buttonid, fill='yellow')   
 
+class ClickPatch:
+    global pd
+    def __init__(self, canvas, name, x, y):
+        self.name = name
+        self.x = x
+        self.y = y
+        self.textid = canvas.create_text(x, y, font="Purisa", text=self.name)
+        canvas.tag_bind(self.textid, '<ButtonRelease-1>', self.onclick)
+    def onclick(self, event):
+        print self.name
+        pd.kill()
+        pd = subprocess.Popen(["/Applications/Pd-0.46-4.app/Contents/MacOS/Pd", "/Users/owen/pd-patches-dev/patches/mother.pd", self.name])
+        
+#oh my god why didnt he ... what?
+
+
 def onchange(name, val) :
     print  str(name) + ": " + str(val)
     sock.sendto(str(name) + " " + str(val) + ";\n", (UDP_IP, UDP_PORT))
 
 
-
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 
- 
-root = Tk()
-canv = Canvas(root, width=800, height=600)
+root = Tkinter.Tk()
+canv = Tkinter.Canvas(root, width=800, height=600)
 
 kx = -80
 ky = -50
 
-button1 =  Button(canv, 'key 1', 100 + kx, 500 + ky, 40, 100, onchange)
-button2 =  Button(canv, 'key 2', 125 + kx, 390 + ky, 40, 100, onchange)
-button3 =  Button(canv, 'key 3', 150 + kx, 500 + ky, 40, 100, onchange)
-button4 =  Button(canv, 'key 4', 175 + kx, 390 + ky, 40, 100, onchange)
-button5 =  Button(canv, 'key 5', 200 + kx, 500 + ky, 40, 100, onchange)
-button6 =  Button(canv, 'key 6', 250 + kx, 500 + ky, 40, 100, onchange)
-button7 =  Button(canv, 'key 7', 275 + kx, 390 + ky, 40, 100, onchange)
-button8 =  Button(canv, 'key 8', 300 + kx, 500 + ky, 40, 100, onchange)
-button9 =  Button(canv, 'key 9', 325 + kx, 390 + ky, 40, 100, onchange)
-button10 = Button(canv, 'key 10', 350 + kx, 500 + ky, 40, 100, onchange)
-button11 = Button(canv, 'key 11', 375 + kx, 390 + ky, 40, 100, onchange)
-button12 = Button(canv, 'key 12', 400 + kx, 500 + ky, 40, 100, onchange)
-button13 = Button(canv, 'key 13', 450 + kx, 500 + ky, 40, 100, onchange)
-button14 = Button(canv, 'key 14', 475 + kx, 390 + ky, 40, 100, onchange)
-button15 = Button(canv, 'key 15', 500 + kx, 500 + ky, 40, 100, onchange)
-button16 = Button(canv, 'key 16', 525 + kx, 390 + ky, 40, 100, onchange)
-button17 = Button(canv, 'key 17', 550 + kx, 500 + ky, 40, 100, onchange)
-button18 = Button(canv, 'key 18', 575 + kx, 390 + ky, 40, 100, onchange)
-button19 = Button(canv, 'key 19', 600 + kx, 500 + ky, 40, 100, onchange)
-button20 = Button(canv, 'key 20', 650 + kx, 500 + ky, 40, 100, onchange)
-button21 = Button(canv, 'key 21', 675 + kx, 390 + ky, 40, 100, onchange)
-button22 = Button(canv, 'key 22', 700 + kx, 500 + ky, 40, 100, onchange)
-button23 = Button(canv, 'key 23', 725 + kx, 390 + ky, 40, 100, onchange)
-button24 = Button(canv, 'key 24', 750 + kx, 500 + ky, 40, 100, onchange)
+button1 =  Key(canv, 'key 1', 100 + kx, 500 + ky, 40, 100, onchange)
+button2 =  Key(canv, 'key 2', 125 + kx, 390 + ky, 40, 100, onchange)
+button3 =  Key(canv, 'key 3', 150 + kx, 500 + ky, 40, 100, onchange)
+button4 =  Key(canv, 'key 4', 175 + kx, 390 + ky, 40, 100, onchange)
+button5 =  Key(canv, 'key 5', 200 + kx, 500 + ky, 40, 100, onchange)
+button6 =  Key(canv, 'key 6', 250 + kx, 500 + ky, 40, 100, onchange)
+button7 =  Key(canv, 'key 7', 275 + kx, 390 + ky, 40, 100, onchange)
+button8 =  Key(canv, 'key 8', 300 + kx, 500 + ky, 40, 100, onchange)
+button9 =  Key(canv, 'key 9', 325 + kx, 390 + ky, 40, 100, onchange)
+button10 = Key(canv, 'key 10', 350 + kx, 500 + ky, 40, 100, onchange)
+button11 = Key(canv, 'key 11', 375 + kx, 390 + ky, 40, 100, onchange)
+button12 = Key(canv, 'key 12', 400 + kx, 500 + ky, 40, 100, onchange)
+button13 = Key(canv, 'key 13', 450 + kx, 500 + ky, 40, 100, onchange)
+button14 = Key(canv, 'key 14', 475 + kx, 390 + ky, 40, 100, onchange)
+button15 = Key(canv, 'key 15', 500 + kx, 500 + ky, 40, 100, onchange)
+button16 = Key(canv, 'key 16', 525 + kx, 390 + ky, 40, 100, onchange)
+button17 = Key(canv, 'key 17', 550 + kx, 500 + ky, 40, 100, onchange)
+button18 = Key(canv, 'key 18', 575 + kx, 390 + ky, 40, 100, onchange)
+button19 = Key(canv, 'key 19', 600 + kx, 500 + ky, 40, 100, onchange)
+button20 = Key(canv, 'key 20', 650 + kx, 500 + ky, 40, 100, onchange)
+button21 = Key(canv, 'key 21', 675 + kx, 390 + ky, 40, 100, onchange)
+button22 = Key(canv, 'key 22', 700 + kx, 500 + ky, 40, 100, onchange)
+button23 = Key(canv, 'key 23', 725 + kx, 390 + ky, 40, 100, onchange)
+button24 = Key(canv, 'key 24', 750 + kx, 500 + ky, 40, 100, onchange)
 
-
-
-
+auxbutton = Key(canv, 'aux', 500, 100, 50, 50, onchange)
 
 slider1 = Slider(canv, 's1',  20, 10, 80, 265, onchange)
 slider2 = Slider(canv, 's2', 120, 10, 80, 265, onchange)
 slider3 = Slider(canv, 's3', 220, 10, 80, 265, onchange)
 slider4 = Slider(canv, 's4', 320, 10, 80, 265, onchange)
 
+# load patches
+print " ... loading patches ... "
+patch_names = []
+patch_folders = get_immediate_subdirectories('./patches/')
+i = 0
+for patch_folder in patch_folders :
+    patch_name = str(patch_folder)
+    patch_path = '/Users/owen/pd-patches-dev/patches/' + patch_name + '/main.pd'
+    i = i+1
+    print patch_path
+  #  try :
+  #      patch_names.append(ClickPatch(canv, patch_path, 600, 20 * i))
+  #  except Exception, e:
+  #      print traceback.format_exc()
+        
+
+print patch_names
+
 canv.pack()
+
 root.mainloop()
 
